2d82f8382ff8468febdf72862ac1fb1715cfd0bf
ShaotuJia <shaotu.jia@hotmail.com>
2017-11-04 08:52:37 -0400

    #10 logClient: create a client logClient in node listener to call log_level service at the beginning'

--- src/listener.cpp
+++ src/listener.cpp
@@ -10,11 +10,18 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "beginner_tutorials/Level.h"
+
+// global variable
+int listener_count = 0;
 
 // A function receive messages over the ROS system
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
 	// post ROS_INFO
 	ROS_INFO("I heard from talker: [%s]", msg->data.c_str());
+
+	//increment listener_count
+	listener_count ++;
 }
 
 int main(int argc, char **argv) {
@@ -23,6 +30,23 @@ int main(int argc, char **argv) {
 
 	// Create nodehandle
 	ros::NodeHandle n;
+
+	// Create a client object for log_level service
+	ros::ServiceClient logClient = n.serviceClient<beginner_tutorials::Level>("log_level");
+	beginner_tutorials::Level::Request req;
+	beginner_tutorials::Level::Response resp;
+
+	// set up value for req
+	req.count = listener_count;
+
+	// actually call the service
+	bool success = logClient.call(req, resp);
+
+	if (success) {
+		ROS_INFO_STREAM(resp.message);
+	} else {
+		ROS_ERROR_STREAM("Failed to call log_level");
+	}
 
 	// subscribe topic std_msgs/String
 	ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
